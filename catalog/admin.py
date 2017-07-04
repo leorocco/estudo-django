645ce7767cc45f1af517037030c61387a34ef54f
Leonardo Rocco <leeo.rocco@gmail.com>
2017-07-04 11:36:57 -0300

    aula 20 finalizada

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,5 +5,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, Category
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created', 'modified']
+    search_fields = ['name', 'slug']
+    list_filter = ['created', 'modified']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'created', 'modified']
+    search_fields = ['name', 'slug', 'category__name']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'core',
+    'catalog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #APP_DIRS'
+                'catalog.context_processors.categories',
             ],
         },
     },
