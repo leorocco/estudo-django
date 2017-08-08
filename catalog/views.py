42fdbc20fa0dab78f078c2d43c3995527fc381f1
LEONARDO ROCCO <l.rocco@ifsp.edu.br>
2017-08-08 14:02:02 -0300

    ajeitando o envio do contexto em produtos

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,4 @@ def product_list(request):
     context = {
         'product_list' : Product.objects.all()
     }
-    return render(request, 'catalog/product_list.html')
+    return render(request, 'catalog/product_list.html', context)
